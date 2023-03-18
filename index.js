1d9c3b290f3236e175bd9ca05a03706a1834b11f
Jack Shao <jacky.shaoxg@gmail.com>
2023-03-18 11:44:51 +0000

    just always use vuecli

--- index.js
+++ index.js
@@ -127,11 +127,11 @@ const runAction = () => {
 	}
 
 	log(`Building${release ? " and releasing" : ""} the Electron app…`);
-	const cmd = useVueCli ? "vue-cli-service electron:build" : "electron-builder";
+	//const cmd = useVueCli ? "vue-cli-service electron:build" : "electron-builder";
 	for (let i = 0; i < maxAttempts; i += 1) {
 		try {
 			run(
-				`${useNpm ? "npx --no-install" : "yarn run"} ${cmd} --${platform} ${
+				`${useNpm ? "npm run" : "yarn run"} electron:build --${platform} ${
 					release ? "--publish always" : ""
 				} ${args}`,
 				appRoot,
